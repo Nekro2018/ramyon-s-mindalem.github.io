3618eb265c4bdbcaf4b64a15952bff72a1bb2920
Nekro2018 <108792770+Nekro2018@users.noreply.github.com>
2022-07-06 13:07:33 +0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="ru-RUS">
+<head>
+<title>RSM - О нас</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About" />
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>О нас</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Немного информации о команде!
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Наши тайтлы</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+								План идеального финала сказки для злодейки; Магистр дьявольского культа – Сорока; Траектория движения Венеры; Большой начальник; Кофе и ваниль — темнее чёрного; Необычный любовник; Мой влиятельный парень! Магистр дьявольского культа: Роза в ожидании; Королевская Битва; Твой милый котик-подражатель!; Красивый волк; Вчерашний день был подобен смерти; Тающие губы; Юность Сукебе; Я Хочу увидеть звёзды; Это должен быть он; На пути к твоему сердцу; Сонбэ, не крась губы; Мое одолженное тело; Он популярен, но не умеет любить; Цветы не цветут; Этот парень в классе; Мой нулевой фанат; Как же мы любим; Волнующий брак: Путь к твоему сердцу; Королевская битва 2: Блиц Рояль; Парадная дверь; Причинная связь; Туфли Золушки; Кшитигарбха Востока. <br>
+									<br></p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class='splitter-con-container'>
+
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Наши Админы</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Главный Админ - Миндаль на связи. Админ - Минмина; Хомячок.
+									<br>
+
+									<br>
+								</p>
+                                </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Участники команды</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Миндаль на связи; Минмина; Хомячок; Адема; Даха; Nutellka; Vika; Богиня_вина; Mentalrepairs; Чертенок; Змейка; НекромЭн; Миленькая Кошкодевочка; Хз кто я; Benneli; Ancyxen; Точка; Яойчик; Y/n; Сырок; Mad; Юми.<br>
+									<br>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+</div>
+</body>
+</html>
---
+++ lib/module/inc-menu.php
@@ -0,0 +1,217 @@
+<div class='menu-con-release'>
+	<span>Website Release Preview 2021.12.05</span>
+</div>
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+		<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>This feature is currently not yet available</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Темная Тема</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-transparency">
+		<div class="settings-menu-ico-transparency">
+		</div>
+		<div class="settings-menu-btn-button btn-transparency">
+			<span>Прозрачность</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-particles">
+		<div class="settings-menu-ico-particles">
+		</div>
+		<div class="settings-menu-btn-button btn-particles">
+			<span>Частицы</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-pulsate">
+		<div class="settings-menu-ico-pulsate">
+		</div>
+		<div class="settings-menu-btn-button btn-pulsate">
+			<span>Пульсация</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-waves">
+		<div class="settings-menu-ico-waves">
+		</div>
+		<div class="settings-menu-btn-button btn-waves">
+			<span>Волны</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-theme settings-menu-lower" style="border-top: solid 1px rgba(0,0,0,.02);">
+		<div class="settings-menu-ico-theme">
+		</div>
+		<div class="settings-menu-btn-button btn-theme">
+			<span>Выбери тему</span>
+		</div>
+	</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			<a href='/about'>
+			<div class="menu-btn-select">
+				<span>О нас</span>
+			</div>
+			</a>
+			<div class="menu-con-divider">
+			</div>
+			<a href='https://mangalib.me/team/ramyon-s-mindalem' target="_blank">
+			<div class="menu-btn-select">
+				<span>MangaLib</span>
+			</div>
+			</a>
+			<a href='https://readmanga.io/list/person/ramen_s_mindalem' target="_blank">
+			<div class="menu-btn-select">
+				<span>ReadManga</span>
+			</div>
+			</a>
+			<a href='https://mintmanga.live/list/person/ramen_s_mindalem'>
+			<div class="menu-btn-select">
+				<span>MintManga</span>
+			</div>
+			</a>
+			<a href='https://t.me/+1bH7UVFt-2IzY2Ey' target="_blank">
+			<div class="menu-btn-select">
+				<span>Telegram</span>
+			</div>
+			</a>
+			<a href='https://vk.com/rsmbdsm' target="_blank">
+			<div class="menu-btn-select">
+				<span>Вконтакте</span>
+			</div>
+			</a>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Главная</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Домой</span>
+				</div>
+				</a>
+				<a href="/about">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>О нас</span>
+				</div>
+				</a>
+				</div>
+				</a>
+				<div class='mobile-menu-tx1-group'>
+					<span>Наши соцсети</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="https://mangalib.me/team/ramyon-s-mindalem" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>MangaLib</span>
+				</div>
+				</a>
+				<a href="https://readmanga.io/list/person/ramen_s_mindalem" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>ReadManga</span>
+				</div>
+				</a>
+				<a href="https://mintmanga.live/list/person/ramen_s_mindalem" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>MintManga</span>
+				</div>
+				</a>
+				<a href="https://t.me/+1bH7UVFt-2IzY2Ey">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Telegram</span>
+				</div>
+				</a>
+				<a href="https://vk.com/rsmbdsm">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Вконтакте</span>
+				</div>
+				</a>
+				<a href="https://discord.gg/HWyhj9wBc9">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Discord</span>
+				</div>
+				</a>
+				<div class='mobile-menu-btn-spacer'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
